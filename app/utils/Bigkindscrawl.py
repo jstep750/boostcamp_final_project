001b965bad89e130dc40af972ba4ab41d08a45d4
hello0U0 <kleu358@gmail.com>
2023-01-20 04:56:02 +0000

    new Bigkindscrwal

--- app/utils/Bigkindscrawl.py
+++ app/utils/Bigkindscrawl.py
@@ -14,10 +14,10 @@ def bigkinds_crawl(company_name:str,date_gte:int,date_lte:int) -> pd.DataFrame:
     '''
 
     #response = List[{'_index','_type','_id','_score', '_source':{'title','description','titleNdescription','URL','date'}}]
-    response = requests.get(f"http://118.67.133.53:30001/search/{company_name}/?&date_gte={date_gte}&date_lte={date_lte}").json()
-    
+    response = requests.get(f"http://27.96.131.161:30001/search/{company_name}/?query_sentence={company_name}&date_gte={date_gte}&date_lte={date_lte}").json()
     # 데이터프레임으로 변환 news_df = ['title','description','titleNdescription','URL','date']
     news_df = defaultdict(list)
+    
     for idx in range(len(response)):
         response_source = response[idx]['_source']
         for key, value in response_source.items():
